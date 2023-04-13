93fb2b907e25889ef6ceefe53246e928aa988ec3
Kiran <kirandp416@gmail.com>
2023-04-13 18:40:54 +0100

    Parameters for negative tests

--- src/test/java/com/herokuapp/theinternet/Test2.java
+++ src/test/java/com/herokuapp/theinternet/Test2.java
@@ -5,11 +5,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Test2 {
+    @Parameters({"username","password","expectedMessage"})
     @Test(priority = 1, groups = { "negativeTests", "smokeTests" })
-    public void invalidUsername(){
+    public void negativeLogin(String username, String password, String expectedMessage){
         System.setProperty("webdriver.chrome.driver","C:/Automation/Selenium Maven/src/main/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         String url = "https://the-internet.herokuapp.com/";
@@ -18,16 +20,15 @@ public class Test2 {
         driver.manage().window().maximize();
 
         driver.findElement(By.xpath("//*[@id=\"content\"]/ul/li[21]/a")).click();
-        driver.findElement(By.name("username")).sendKeys("invalid");
-        driver.findElement(By.name("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.name("username")).sendKeys(username);
+        driver.findElement(By.name("password")).sendKeys(password);
         driver.findElement(By.className("radius")).click();
 
         WebElement errorMessage = driver.findElement(By.className("error"));
 
         String actualError = errorMessage.getText();
-        String expectedError = "Your username is invalid!";
 
-        Assert.assertTrue(actualError.contains(expectedError),"Invalid Error message");
+        Assert.assertTrue(actualError.contains(expectedMessage),"Invalid Error message");
 
         String expectedURL = "https://the-internet.herokuapp.com/login";
         String actualURL = driver.getCurrentUrl();
@@ -38,33 +39,33 @@ public class Test2 {
 
     }
 
-    @Test(priority = 2, groups = { "negativeTests" })
-    public void invalidPassword(){
-        System.setProperty("webdriver.chrome.driver","C:/Automation/Selenium Maven/src/main/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        String url = "https://the-internet.herokuapp.com/";
-        driver.get(url);
-
-        driver.manage().window().maximize();
-
-        driver.findElement(By.xpath("//*[@id=\"content\"]/ul/li[21]/a")).click();
-        driver.findElement(By.name("username")).sendKeys("tomsmith");
-        driver.findElement(By.name("password")).sendKeys("SuperSecretPassword");
-        driver.findElement(By.className("radius")).click();
-
-        WebElement errorMessage = driver.findElement(By.className("error"));
-
-        String actualError = errorMessage.getText();
-        String expectedError = "Your password is invalid!";
-
-        Assert.assertTrue(actualError.contains(expectedError),"Invalid Error message");
-
-        String expectedURL = "https://the-internet.herokuapp.com/login";
-        String actualURL = driver.getCurrentUrl();
-
-        Assert.assertEquals(expectedURL,actualURL);
-
-        driver.quit();
-
-    }
+//    @Test(priority = 2, groups = { "negativeTests" })
+//    public void invalidPassword(){
+//        System.setProperty("webdriver.chrome.driver","C:/Automation/Selenium Maven/src/main/resources/chromedriver.exe");
+//        WebDriver driver = new ChromeDriver();
+//        String url = "https://the-internet.herokuapp.com/";
+//        driver.get(url);
+//
+//        driver.manage().window().maximize();
+//
+//        driver.findElement(By.xpath("//*[@id=\"content\"]/ul/li[21]/a")).click();
+//        driver.findElement(By.name("username")).sendKeys("tomsmith");
+//        driver.findElement(By.name("password")).sendKeys("SuperSecretPassword");
+//        driver.findElement(By.className("radius")).click();
+//
+//        WebElement errorMessage = driver.findElement(By.className("error"));
+//
+//        String actualError = errorMessage.getText();
+//        String expectedError = "Your password is invalid!";
+//
+//        Assert.assertTrue(actualError.contains(expectedError),"Invalid Error message");
+//
+//        String expectedURL = "https://the-internet.herokuapp.com/login";
+//        String actualURL = driver.getCurrentUrl();
+//
+//        Assert.assertEquals(expectedURL,actualURL);
+//
+//        driver.quit();
+//
+//    }
 }
